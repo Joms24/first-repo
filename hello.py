ad23ef7797caff6499a74bcf5dbafbe4e377f306
Joms24 <jomscometa@gmail.com>
2022-07-29 21:38:32 +0800

    1stpython commit

---
+++ hello.py
@@ -0,0 +1,36 @@
+print("Hello World")
+
+anime = ["Naruto", "One piece", "Bleach"]
+
+for best in anime:
+    print(best)
+
+
+for i in range(6):
+    print(i)
+
+s = set()
+
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(5)
+s.remove(2)
+
+print(s)
+
+anime = ["One piece", "Naruto", "Bleach"]
+
+anime.append("Boruto")
+anime.sort
+print(anime)
+
+n = int(input("Number: "))
+
+if n > 0:
+    print("n is positive")
+elif n < 0:
+    print("n is negative")
+else:
+    print("n is zero")
